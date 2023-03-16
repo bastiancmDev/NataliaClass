2d361be5ea78ac1e5058f4cbfeefed83a070ce9a
nateusse <natyeusse@gmail.com>
2023-03-15 20:41:49 -0400

    trying city

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    public CharacterController player;
+    public float playerSpeed = 4.0F;
+    private Vector3 movePlayer;
+    private Vector3 camRight;
+    private Vector3 camForward;
+    private Vector3 playerInput;
+    public Camera mainCam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        player.Move(playerInput * playerSpeed * Time.deltaTime);
+
+
+    }
+
+    void camDirection()
+    {
+        camForward = mainCam.transform.forward;
+        camRight = mainCam.transform.right;
+
+        camForward.y = 0;
+        camRight.z = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+}
